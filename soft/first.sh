03aa4d9e48537c84f90cc92a25af94fa5787f783
王创 <mail.wangchuang@gmail.com>
2018-02-08 21:10:20 +0800

    no message

--- soft/first.sh
+++ soft/first.sh
@@ -1,9 +1,10 @@
 #bin/bash
-if [ -f /tmp/first ];
+if [ -f /tmp/first ]
+then
 	/usr/local/mysql/scripts/mysql_install_db --defaults-file=/etc/my.cnf --basedir=/usr/local/mysql/ --datadir=/usr/local/mysql/data/	--user=mysql 
 	rm -rf /tmp/first
-else
-	/etc/init.d/httpd start
-    /etc/init.d/mysqld start
+fi
+/etc/init.d/httpd start
+/etc/init.d/mysqld start
 
 	
