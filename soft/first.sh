e17c169853aed3962fb0a72920c785f3c8ad6715
wangchuang <mail.wangchuang@gmail.com>
2018-03-06 17:32:12 +0800

    no message

--- soft/first.sh
+++ soft/first.sh
@@ -2,8 +2,8 @@
 if [ -f /tmp/first ]
 then
 	mkdir /usr/local/mysql/data/
-	chmod 775 /usr/loca/mysql/data/
-	chown mysql:mysql /usr/loca/mysql/data/
+	chmod 775 /usr/local/mysql/data/
+	chown mysql:mysql /usr/local/mysql/data/
 	/usr/local/mysql/scripts/mysql_install_db --defaults-file=/etc/my.cnf --basedir=/usr/local/mysql/ --datadir=/usr/local/mysql/data/	--user=mysql 
 	rm -rf /tmp/first
 fi
